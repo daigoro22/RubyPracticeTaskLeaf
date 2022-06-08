1b24addc731e65b8f88dc29640718c89f3570b8a
daigoro22 <abe.daigoro.sb@tut.jp>
2022-06-08 13:43:17 +0900

    Add /login endpoint
    ログイン画面と処理を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login' to: 'session#create'
   root to: 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
